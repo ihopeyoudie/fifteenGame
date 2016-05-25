5ea882a9473ed79b0e6c84e40cf0fbce761aa5c0
dzirt <ihopeyoudie@mai.ru>
2016-05-26 02:01:25 +0400

    version 0.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import net.dzirt.MainFrame;
+
+/**
+ * Created by Dzirt on 25.05.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame1 = new MainFrame();
+        //frame1.init();
+
+    }
+}
